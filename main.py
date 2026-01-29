63e91b0a839ef408c7e2fce5ad0187bec57397b1
fredc19 <f.cuthbertson2@newcastle.ac.uk>
2026-01-29 22:21:33 +0000

    Completed task 1

---
+++ main.py
@@ -0,0 +1,31 @@
+#import modules
+#print modules.generate_full_name("Fred","C")
+from modules import generate_full_name as full_name
+print( full_name('Fred','Cuthbertson'))
+
+# import the module
+import os
+# Creating a directory
+os.mkdir('directory_name')
+# Changing the current directory
+os.chdir('path')
+# Getting current working directory
+os.getcwd()
+# Removing directory
+os.rmdir()
+
+#In general have imports at the top
+from statistics import * # importing all the statistics modules
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))       # ~22.9
+print(median(ages))     # 23
+print(mode(ages))       # 20
+print(stdev(ages))      # ~2.3
+
+import math
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(2))      # 1.4142135623730951, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
--- modules.py
+++ modules.py
@@ -1,2 +1,22 @@
 def generate_full_name(first_name,last_name):
-    return first_name + '' + last_name
+    return first_name + ' ' + last_name
+
+#Modules are just files imported into anoter file so it
+#can use that files functions/ variables.
+
+from random import random, randint
+print(random())   # it doesn't take any arguments; it returns a value between 0 and 0.9999
+print(randint(5, 20)) # it returns a random integer number between [5, 20] inclusive
+
+
+#Basketball 3 point randomness
+makes=0
+miss=0
+attempts = 100
+for x in range(attempts):
+    shot = randint(1,10)
+    if shot <= 4:
+        makes+=1
+    else:
+        miss +=1
+print (f"Makes:{makes} Misses:{miss}")
